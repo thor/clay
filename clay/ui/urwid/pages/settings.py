28246e63557c65419b5914c761f22675b00170f3
Valentijn <valentijn@posteo.net>
2019-07-14 13:05:25 +0200

    Remove the frequency slider

--- clay/ui/urwid/pages/settings.py
+++ clay/ui/urwid/pages/settings.py
@@ -11,120 +11,6 @@ from clay.ui.urwid import hotkey_manager
 
 player = get_player()  # pylint: disable=invalid-name
 
-
-class Slider(urwid.Widget):
-    """
-    Represents a (TODO: vertical) slider for equalizer band modification.
-    TODO: Save equalizer settings in config file.
-    """
-    _sizing = frozenset([urwid.FLOW])
-
-    CHARS = [
-        u'\u2584',
-        u'\u25A0',
-        u'\u2580',
-    ]
-    ZERO_CHAR = u'\u2500'
-
-    def selectable(self):
-        return True
-
-    def __init__(self, index, freq):
-        self.index = index
-        if int(freq) == freq:
-            freq = int(freq)
-        self.freq = freq
-        if freq >= 1000:
-            self.freq_str = str(freq // 1000) + '\nKHz'
-        else:
-            self.freq_str = str(freq) + '\nHz'
-        self.value = 0
-        self.slider_height = 13
-        self.max_value = 20
-        super(Slider, self).__init__()
-
-    def rows(self, *_):
-        """
-        Return count of rows required to render this widget.
-        """
-        return self.slider_height + 3
-
-    def render(self, size, focus=None):
-        """
-        Render widget.
-        """
-        rows = [('+' if self.value > 0 else '') + str(self.value) + ' dB']
-
-        chars = [' '] * self.slider_height
-
-        if self.value == 0:
-            chars[self.slider_height // 2] = Slider.ZERO_CHAR
-        else:
-            k = ((float(self.value) / (self.max_value + 1)) + 1) / 2  # Convert value to [0;1] range
-            section_index = int(k * self.slider_height)
-            char_index = int(k * self.slider_height * len(Slider.CHARS)) % len(Slider.CHARS)
-            chars[section_index] = Slider.CHARS[char_index]
-
-        rows.extend([
-            (
-                u'\u2524{}\u251C'
-                if i == self.slider_height // 2
-                else u'\u2502{}\u2502'
-            ).format(x)
-            for i, x
-            in enumerate(reversed(chars))
-        ])
-        rows.append(self.freq_str)
-        text = urwid.AttrMap(urwid.Text('\n'.join(
-            rows
-        ), align=urwid.CENTER), 'default', 'panel_focus')
-        return text.render(size, focus)
-
-    def keypress(self, _, key):
-        """
-        Handle equalizer band modification.
-        """
-        return hotkey_manager.keypress("settings_page", self, None, None, key)
-
-    def equalizer_up(self):
-        """
-        Turn the equalizer band up
-        """
-        if self.value < self.max_value:
-            self.value += 1
-            self.update()
-
-    def equalizer_down(self):
-        """
-        Turn the equalizer band down
-        """
-        if self.value > -self.max_value:
-            self.value -= 1
-            self.update()
-
-    def update(self):
-        """
-        Update player equalizer & toggle redraw.
-        """
-        player.set_equalizer_value(self.index, self.value)
-        self._invalidate()
-
-
-class Equalizer(urwid.Columns):
-    """
-    Represents an equalizer.
-    """
-    def __init__(self):
-        self.bands = [
-            Slider(index, freq)
-            for index, freq
-            in enumerate(player.get_equalizer_freqs())
-        ]
-        super(Equalizer, self).__init__(
-            self.bands
-        )
-
-
 class SettingsPage(urwid.Columns, AbstractPage):
     """
     Settings page.
@@ -159,7 +45,6 @@ class SettingsPage(urwid.Columns, AbstractPage):
             'Download tracks before playback',
             state=settings_manager.get('download_tracks', 'play_settings') or False
         )
-        self.equalizer = Equalizer()
         super(SettingsPage, self).__init__([urwid.ListBox(urwid.SimpleListWalker([
             urwid.Text('Settings'),
             urwid.Divider(' '),
@@ -176,9 +61,7 @@ class SettingsPage(urwid.Columns, AbstractPage):
             urwid.Divider(' '),
             urwid.AttrWrap(urwid.Button(
                 'Save', on_press=self.on_save
-            ), 'input', 'input_focus'),
-            urwid.Divider(u'\u2500'),
-            self.equalizer,
+            ), 'input', 'input_focus')
         ]))])
 
     def on_save(self, *_):
